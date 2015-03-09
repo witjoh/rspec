fcbd4aa4f4868b5d5376276c73db97e499b135c8
Johan De Wit <johan@koewacht.net>
2015-03-09 22:06:55 +0100

    for the demo_spec, merged all let(:facts) together - we dont have conditionals to test, so we only have one context

--- spec/classes/demo_spec.rb
+++ spec/classes/demo_spec.rb
@@ -2,39 +2,27 @@ require 'spec_helper'
 
 describe 'rspec::demo' do
 
-  context "normal fact" do
-    let(:facts) do
-      { :osfamily => 'RedHat' }
-    end
-
-    it do
-      should contain_notify('normal fact').with({
-        'message' => /RedHat/,
-      })
-    end
+  let(:facts) do
+    { :osfamily => 'RedHat',
+      :os => { 'family' => 'RedHat', 'release' => { 'full' => '10.0' } } 
+    }
   end
 
-  context "Array fact" do
-    let(:facts) do
-      { :os => { 'family' => 'RedHat' } }
-    end
-
-    it do
-      should contain_notify('array fact').with({
-        'message' => /RedHat/,
-      })
-    end
+  it do
+    should contain_notify('normal fact').with({
+      'message' => /RedHat/,
+    })
   end
 
-  context 'Nested fact' do
-    let(:facts) do
-      { :os => { 'release' => { 'full' => '10.0' } } }
-    end
+  it do
+    should contain_notify('array fact').with({
+      'message' => /RedHat/,
+    })
+  end
 
-    it do
+  it do
     should contain_notify('nested fact').with({
-        'message' => /10.0/,
-      })
-    end
+      'message' => /10.0/,
+    })
   end
 end
